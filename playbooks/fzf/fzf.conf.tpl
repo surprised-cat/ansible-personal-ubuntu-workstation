165456202e80fc40cde607a4dc6a15428b21fd13
andrii.lavrov <andrii.lavrov@aerlingus.com>
2025-09-09 12:51:22 +0200

    feat: Add ripgrep fzf usage

--- playbooks/fzf/fzf.conf.tpl
+++ playbooks/fzf/fzf.conf.tpl
@@ -2,3 +2,4 @@ PATH=$PATH:{{ fzf_sourcecode_dir }}/bin
 eval "$(fzf --bash)"
 source {{ fzf_extensions_dir}}/fzf-git/fzf-git.sh
 export FZF_DEFAULT_OPTS="--height=40% --layout=reverse --info=inline --border" 
+alias rfv='{{ fzf_extensions_dir}}/fzf_ripgrep_interactive.sh'
---
+++ playbooks/fzf/fzf_ripgrep_interactive.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# 1. Search for text in files using Ripgrep
+# 2. Interactively restart Ripgrep with reload action
+# 3. Open the file in Vim (commented)
+# see https://github.com/junegunn/fzf/blob/master/ADVANCED.md#using-fzf-as-interactive-ripgrep-launcher
+RG_PREFIX="rg --column --line-number --no-heading --color=always --smart-case "
+INITIAL_QUERY="${*:-}"
+fzf --ansi --disabled --query "$INITIAL_QUERY" \
+    --bind "start:reload:$RG_PREFIX {q}" \
+    --bind "change:reload:sleep 0.1; $RG_PREFIX {q} || true" \
+    --delimiter : \
+    --preview 'cat {1}' 
+    #--bind 'enter:become(vim {1} +{2})'
