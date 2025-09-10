a730b6f2b0f7b63cb278888f625e93dafa2cf581
andrii.lavrov <andrii.lavrov@aerlingus.com>
2025-09-10 13:19:30 +0200

    feat: make fzf ripgrep center on matches

--- playbooks/fzf/fzf.conf.tpl
+++ playbooks/fzf/fzf.conf.tpl
@@ -1,5 +1,13 @@
 PATH=$PATH:{{ fzf_sourcecode_dir }}/bin
 eval "$(fzf --bash)"
 source {{ fzf_extensions_dir}}/fzf-git/fzf-git.sh
-export FZF_DEFAULT_OPTS="--height=40% --layout=reverse --info=inline --border" 
+
+export FZF_DEFAULT_OPTS="--height=40% \
+ --layout=reverse \
+ --info=inline \
+ --border \
+ --preview='batcat --color=always {1}' \
+ --preview-window=right,60%,border-left \
+ --bind=ctrl-k:preview-up,ctrl-j:preview-down,ctrl-h:preview-half-page-up,ctrl-l:preview-half-page-down" 
+
 alias rfv='{{ fzf_extensions_dir}}/fzf_ripgrep_interactive.sh'
--- playbooks/fzf/fzf_ripgrep_interactive.sh
+++ playbooks/fzf/fzf_ripgrep_interactive.sh
@@ -5,10 +5,14 @@
 # 3. Open the file in Vim (commented)
 # see https://github.com/junegunn/fzf/blob/master/ADVANCED.md#using-fzf-as-interactive-ripgrep-launcher
 RG_PREFIX="rg --column --line-number --no-heading --color=always --smart-case "
+PREVIEW_KEY_BINDINGS="ctrl-k:preview-up,ctrl-j:preview-down,ctrl-h:preview-half-page-up,ctrl-l:preview-half-page-down"
+# last options in preview-window param respond for centering preview on mathing string
 INITIAL_QUERY="${*:-}"
-fzf --ansi --disabled --query "$INITIAL_QUERY" \
+fzf --ansi \
+    --disabled \
+    --query "$INITIAL_QUERY" \
     --bind "start:reload:$RG_PREFIX {q}" \
-    --bind "change:reload:sleep 0.1; $RG_PREFIX {q} || true" \
+    --bind "change:reload:sleep 0.1; $RG_PREFIX {q} || true,$PREVIEW_KEY_BINDINGS" \
     --delimiter : \
-    --preview 'batcat --color=always {1} --highlight-line {2}' \
-    #--bind 'enter:become(vim {1} +{2})'
+    --preview-window 'right,60%,border-left,+{2}+3/3,~3' \
+    --preview 'batcat --color=always {1} --highlight-line {2}' 
