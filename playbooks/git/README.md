8e75edaacbff7fc2d73dbafb9629bbc40c9e3416
andrii.lavrov <andrii.lavrov@aerlingus.com>
2025-09-12 18:01:51 +0200

    feat: add git delta conf

---
+++ playbooks/git/README.md
@@ -0,0 +1,5 @@
+add this to .gitconfigure while it is not managed by ansible
+[include]
+    path = ~/git_conf/themes.gitconfig
+[include]
+    path = ~/git_conf/delta.gitconfig
--- playbooks/nvim/lua/config/fzf.lua
+++ playbooks/nvim/lua/config/fzf.lua
@@ -11,5 +11,19 @@ require('fzf-lua').setup {
     fzf = {
       ["ctrl-a"]       = 'select-all',
     },
+
+    -- This is required to make delta be invoked by git, not fzf
+    -- if invoked by fzf it fails to display word based diff and renders diff always by line
+    -- I.E even if 1 symbol is changed the whole line diff is displayed(like default behavior)
+    git = {
+      git = {
+        status = {
+          pager = true,
+        },
+        diff = {
+          pager = true,
+        },
+      }
+    }
   }
 }
